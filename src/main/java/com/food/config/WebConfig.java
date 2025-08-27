ea4a19f87dd6d21f18d2b01a11a14e6636d7dd4e
Kalaiyarasan1619 <155789550+Kalaiyarasan1619@users.noreply.github.com>
2025-08-27 19:50:15 +0530

    api fix

--- src/main/java/com/food/config/WebConfig.java
+++ src/main/java/com/food/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:8081", "http://192.168.155.253:8080") 
+                        .allowedOrigins("http://localhost:8081", "http://192.168.155.253:8080","https://swiggy-backend-wgqs.onrender.com") 
                         // Expo local dev origins
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
